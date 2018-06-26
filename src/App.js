47fa17bad44ced7c4e9618c3b38f1f4bf2ea2fdc
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-06-26 15:40:31 -0500

    Chat box structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,36 @@ export default class App extends Component {
     render() {
         return (
             <div>
-            <h1>React firebase chat</h1>
+                <h1>React firebase chat</h1>
+                <section name="chat-box">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>user1</td>
+                                <td>msg</td>
+                            </tr>
+                            <tr>
+                                <td>user1</td>
+                                <td>msg</td>
+                            </tr>
+                            <tr>
+                                <td>user2</td>
+                                <td>msg</td>
+                            </tr>
+                            <tr>
+                                <td>user1</td>
+                                <td>msg</td>
+                            </tr>
+                            <tr>
+                                <td>user2</td>
+                                <td>msg</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <input type="text" id="username" placeholder="Type your username"/>
+                    <input type="text" id="message" placeholder="Type your message..." />
+                    <input type="button" value="Send" />
+                </section>
             </div>
         )
     }
