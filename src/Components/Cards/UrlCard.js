75a50dc1c428b04fc203dcbdfa585f84f3269582
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-07-04 15:37:42 -0500

    show site instead of full url on card

--- src/Components/Cards/UrlCard.js
+++ src/Components/Cards/UrlCard.js
@@ -4,11 +4,12 @@ export default class ExternalChatMessage extends Component {
 
     render() {
         let link = this.props.link;
+        let host = new window.URL(link.url);
         return (
             <div>
                 <span>{link.title}</span><br />
                 <span>{link.description}</span><br />
-                <span>{link.url}</span><br />
+                <span>{`${host.protocol}//${host.hostname}`}</span><br />
                 <img src={link.image} alt="Not found" height="80" />
             </div>
         )
