99d88a7afe9c17c543c0c35df8e11eafa15713dc
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-07-10 15:13:22 -0500

    modal for videos

--- src/Components/Cards/VidCard.jsx
+++ src/Components/Cards/VidCard.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import icon from './assets/videoIcon.svg';
+import { Modal } from 'antd';
 
-function VidCard(props) {
-    let vid = props.vid;
-    if (vid.source) {
-        // if not controls, show static image... 
-        return (
-            <div>
-                <video src={vid.source} width="220" type="video/mp4" controls />
-            </div>
-        );
-    } else {
-        return (
-            <div>
-                <span>{vid.name}</span><br />
-                <img src={icon} alt="Not found" height="80" />
-            </div>
-        );
+export default class VidCard extends Component {
+    state = {
+        modal: false,
+    }
+
+    setModal = (modal) => {
+        this.setState({ modal });
+    }
+    render() {
+        let vid = this.props.vid;
+        if (vid.source) {
+            return (
+                <div>
+                    <video onClick={() => this.setModal(true)} src={vid.source} width="220" type="video/mp4" />
+                    <Modal
+                        title="Video"
+                        wrapClassName="vertical-center-modal"
+                        visible={this.state.modal}
+                        onOk={() => this.setModal(false)}
+                        onCancel={() => this.setModal(false)}
+                        footer={null}
+                        width={760}
+                    >
+                        <video src={vid.source} type="video/mp4" width="720" controls />
+                    </Modal>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <span>{vid.name}</span><br />
+                    <img src={icon} alt="Not found" height="80" />
+                </div>
+            );
+        }
     }
 }
-
-export default VidCard;
